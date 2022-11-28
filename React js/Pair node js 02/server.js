47ef2afe277d7ca9a8087eb9d4f459aaa2cee007
Alejandro Agra <aleagra08@hotmail.com>
2022-11-28 16:21:07 -0300

    NODE JS 02

--- React js/Pair node js 02/server.js
+++ React js/Pair node js 02/server.js
@@ -1,21 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 const userRouter = require("./routes/users.route");
 const productRouter = require("./routes/product.route");
-const mongose = require("mongoose");
-mongose
-  .connect("mongodb://127.0.0.1:27017/tienda")
-  .then(() => console.log("Conect MongoDB"))
-  .catch((err) => {
-    console.error(err);
-  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(userRouter);
 app.use(productRouter);
-
 app.listen(port, () => {
-  console.log(`Servidor corriendo por el puerto ${port}`);
+  console.log("Servidor corriendo por el puerto " + port);
 });
