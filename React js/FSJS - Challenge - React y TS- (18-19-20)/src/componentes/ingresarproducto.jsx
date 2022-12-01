267e1734fe739ea06d1c2b71c14efa800eabd4b7
Alejandro Agra <aleagra08@hotmail.com>
2022-12-01 12:55:54 -0300

    axios

--- React js/FSJS - Challenge - React y TS- (18-19-20)/src/componentes/ingresarproducto.jsx
+++ React js/FSJS - Challenge - React y TS- (18-19-20)/src/componentes/ingresarproducto.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { MDBCol, MDBRow } from "mdb-react-ui-kit";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
+const url = "http://localhost:3030/product/post";
 export function Ingresarproducto() {
+  const [titulo_producto, setArchivos] = useState("");
+  const [precio_producto, setPrecio] = useState("");
+  const [tipo_producto, setProducto] = useState("celular");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(url, {
+        titulo_producto,
+        precio_producto,
+        tipo_producto,
+      });
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <>
       <section className="form center p-0">
@@ -9,19 +28,31 @@ export function Ingresarproducto() {
           <div className="">
             <h1 className="ingresar-title">INGRESAR NUEVO PRODUCTO</h1>
           </div>
-          <form className="formulario session">
+          <form className="formulario session" onSubmit={handleSubmit}>
             <MDBRow className="mb-3 center">
               <MDBCol className="text-center">
                 <label className="form-label label-ingresar">
                   NOMBRE DEL PRODUCTO
                 </label>
-                <input className="form-control ingresar" type="text" />
+                <input
+                  className="form-control ingresar"
+                  type="text"
+                  id="titulo_producto"
+                  value={titulo_producto}
+                  onChange={(e) => setArchivos(e.target.value)}
+                />
               </MDBCol>
             </MDBRow>
             <MDBRow className="mb-3 ">
               <MDBCol className="text-center">
                 <label className="form-label label-ingresar">PRECIO</label>
-                <input className="form-control ingresar" type="text" />
+                <input
+                  className="form-control ingresar"
+                  type="text"
+                  id="precio_producto"
+                  value={precio_producto}
+                  onChange={(e) => setPrecio(e.target.value)}
+                />
               </MDBCol>
             </MDBRow>
             <MDBRow className="mb-3 ">
@@ -29,11 +60,19 @@ export function Ingresarproducto() {
                 <label className="form-label label-ingresar">
                   TIPO DE PRODUCTO
                 </label>
-                <Form.Select aria-label="Default select example">
+                <Form.Select
+                  aria-label="Default select example"
+                  id="tipo_producto"
+                  onChange={(e) => {
+                    setProducto(e.target.value);
+                    console.log(tipo_producto);
+                  }}
+                >
                   <option className="form-label"></option>
-                  <option value="1">Celular</option>
-                  <option value="2">Televisor</option>
-                  <option value="3">Tablet</option>
+
+                  <option value="Celular">Celular</option>
+                  <option value="Televisor">Televisor</option>
+                  <option value="Tablet">Tablet</option>
                 </Form.Select>
               </MDBCol>
             </MDBRow>
@@ -48,7 +87,7 @@ export function Ingresarproducto() {
               </MDBCol>
             </MDBRow>
             <div className="login">
-              <button className="btn session-btn" type="sumbit">
+              <button className="btn session-btn" type="submit">
                 Enviar
               </button>
               {/* <button onClick={mostrar}>mostrar</button> */}
